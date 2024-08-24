251c9dc26125595e05b6c771061d2d2dde79605d
fede2570 <fede88662@gmail.com>
2024-08-23 18:35:14 -0600

    update

---
+++ Clase 5/Ejemplo2/src/Ejemplo2/EjemploObjetoPersonas.java
@@ -0,0 +1,15 @@
+package Ejemplo2;
+
+import java.util.ArrayList;
+
+public class EjemploObjetoPersonas {
+
+    public static ArrayList<Persona> lista_personas = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dashboard ventana = new Dashboard();
+        ventana.setVisible(true);
+    }
+    
+}
---
+++ Clase 5/Ejemplo2/src/Ejemplo2/Persona.java
@@ -0,0 +1,13 @@
+package Ejemplo2;
+
+public class Persona {
+    
+    int carnet;
+    String nombre;
+    
+    public Persona(int carnet, String nombre) {
+        this.carnet = carnet;
+        this.nombre = nombre;
+    }
+    
+}
