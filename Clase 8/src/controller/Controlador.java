50676bfd9c8f2a6ca0c1ab79967440c3ecae128a
fede2570 <fede88662@gmail.com>
2024-09-17 20:55:20 -0600

    update

--- Clase 8/src/controller/Controlador.java
+++ Clase 8/src/controller/Controlador.java
@@ -37,11 +37,12 @@ public class Controlador implements ActionListener {
     }
     
     public int tiempoMaterial(String material) {
-        int tiempo = 0;
+        int tiempo = -1;
         
         switch (material) {
             case "metal": {
                 tiempo = 5;
+                break;
             }
         }
         return tiempo;
